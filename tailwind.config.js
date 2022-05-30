075f8a791194d4dbfbfaac260a257552ec96801f
Ilshat Sultanov <ilshat@sultanov.team>
2022-05-31 02:41:58 +0500

    Add tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content:
+    process.env.NODE_ENV === "production"
+      ? ["./public/**/*.html", "./public/js/app.js"]
+      : ["./public/**/*.html", "./public/js/app.js", "./public/js/cljs-runtime/*.js"],
+  theme: {
+    extend: {
+      backgroundImage: {},
+    },
+  },
+  darkMode: "class",
+  variants: {},
+  plugins: [],
+};
