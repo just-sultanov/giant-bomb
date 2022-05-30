a60985e988a45f5ade4ae706bee74ef9678934a4
Ilshat Sultanov <ilshat@sultanov.team>
2022-05-31 02:41:25 +0500

    Add karma configuration

---
+++ karma.config.js
@@ -0,0 +1,33 @@
+process.env.CHROME_BIN = require("puppeteer").executablePath();
+
+module.exports = function (config) {
+  config.set({
+    singleRun: true,
+    browsers: ["ChromeHeadless"],
+    basePath: "public",
+    files: ["test/assets/ci.js"],
+    frameworks: ["cljs-test"],
+    plugins: ["karma-cljs-test", "karma-chrome-launcher", "karma-junit-reporter", "karma-coverage"],
+    colors: true,
+    logLevel: config.LOG_INFO,
+    client: {
+      args: ["shadow.test.karma.init"],
+      singleRun: true,
+    },
+
+    reporters: ["junit", "coverage", "progress"],
+    preprocessors: {
+      'test/assets/*.js': ['coverage']
+    },
+    junitReporter: {
+      outputDir: "test/reports/junit",
+      outputFile: undefined,
+      suite: "",
+    },
+    coverageReporter: {
+      type: "lcov",
+      dir: "test/reports/coverage",
+      subdir: ".",
+    },
+  });
+};
