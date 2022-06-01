d40bb763827bc22e9ba9e064e51ee3f4666b9171
Ilshat Sultanov <ilshat@sultanov.team>
2022-06-01 18:25:23 +0500

    Add bean helper functions

---
+++ src/main/clojure/giant_bomb/helpers/bean.cljs
@@ -0,0 +1,24 @@
+(ns giant-bomb.helpers.bean
+  (:require
+    [cljs-bean.core :as b]))
+
+
+(defn ->js
+  [x]
+  (b/->js x))
+
+
+(defn ->clj
+  [x]
+  (b/->clj x))
+
+
+(defn bean
+  [^js x]
+  (when (some? x)
+    (b/bean x :recursive true)))
+
+
+(defn then-bean
+  [^js promise]
+  (.then promise bean))
